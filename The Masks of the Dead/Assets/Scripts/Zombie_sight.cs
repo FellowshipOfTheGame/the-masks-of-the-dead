0083225688158b66d88981d0a9663762c287daff
Newairs <gabriel_arantes18@hotmail.com>
2019-10-22 17:38:14 -0300

    Added basic sight script
    Added script that implements enemie sight, allowing for the zombie to see and track the player

---
+++ The Masks of the Dead/Assets/Scripts/Zombie_sight.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_sight : MonoBehaviour {
+
+    public float fielOfVision = 110f;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+
+    private Pathfinding nav;
+    private SphereCollider col;
+    private GameObject player;
+    private Vector3 previousSighting;
+
+	void Awake () {
+        nav = GetComponent<Pathfinding>();
+        col = GetComponent<SphereCollider>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInSight = false;
+
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+
+            if(angle < fielOfVision * 0.5f)
+            {
+                RaycastHit Ray;
+
+                if(Physics.Raycast(transform.position + transform.up, direction.normalized, out Ray, col.radius))
+                {
+                    if(Ray.collider.gameObject == player)
+                    {
+                        playerInSight = true;
+                        personalLastSighting = player.transform.position;
+                    }
+                }
+            }
+        }
+    }
+}
