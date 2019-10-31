2e6f0b9da4b97b6f74f61133f1c67b90d5a362ec
Newairs <gabriel_arantes18@hotmail.com>
2019-10-31 03:45:55 -0300

    AG script created
    Created basic script for genetic evolution

--- The Masks of the Dead/Assets/Scripts/AG/AG.cs
+++ The Masks of the Dead/Assets/Scripts/AG/AG.cs
@@ -1,16 +1,90 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class AG : MonoBehaviour {
+public class AG<T> {
+    public List<Individuo<T>> Populacao { get; private set; }
+    public int Geracao { get; private set; }
+    public float MelhorFitness { get; private set; }
+    public T[] MelhorGene { get; private set; }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float TaxaMutacao;
+
+    private Random random;
+    private float SomaFitness;
+
+    public AG(int tamanhoPopulacao, int individuoTamanho, Random random, Func<T> GeraGeneAleatorio, Func<float, int> FuncaoFitness, float taxaMutacao = 0.01f)
+    {
+        Geracao = 1;
+        TaxaMutacao = taxaMutacao;
+        Populacao = new List<Individuo<T>>();
+        this.random = random;
+
+        for(int i = 0; i < tamanhoPopulacao; i++)
+        {
+            Populacao.Add(new Individuo<T>(individuoTamanho, random, GeraGeneAleatorio, FuncaoFitness, Inicializa: true));
+        }
+    }
+
+    public void NovaGeracao()
+    {
+        if(Populacao.Count <= 0)
+        {
+            return;
+        }
+
+        CalculaFitness();
+
+        List<Individuo<T>> novaPopulacao = new List<Individuo<T>>();
+
+        for(int i = 0; i < Populacao.Count; i++)
+        {
+            Individuo<T> Pai1 = EscolhePai();
+            Individuo<T> Pai2 = EscolhePai();
+
+            Individuo<T> Filho = Pai1.Crossover(Pai2);
+
+            Filho.Mutacao(TaxaMutacao);
+
+            novaPopulacao.Add(Filho);
+        }
+
+        Populacao = novaPopulacao;
+        Geracao++;
+    }
+
+    public void CalculaFitness()
+    {
+        SomaFitness = 0;
+        Individuo<T> Melhor = Populacao[0];
+
+        for(int i = 0; i < Populacao.Count; i++)
+        {
+            SomaFitness += Populacao[i].CalculoFitness(i);
+
+            if(Populacao[i].Fitness > Melhor.Fitness)
+            {
+                Melhor = Populacao[i];
+            }
+        }
+
+        MelhorFitness = Melhor.Fitness;
+        Melhor.Genes.CopyTo(MelhorGene, 0);
+    }
+
+    private Individuo<T> EscolhePai()
+    {
+        double Aleatoriedade = random.NextDouble() * SomaFitness;
+
+        for (int i = 0; i < Populacao.Count; i++)
+        {
+            if(Aleatoriedade < Populacao[i].Fitness)
+            {
+                return Populacao[i];
+            }
+
+            Aleatoriedade -= Populacao[i].Fitness;
+        }
+
+        return null;
+    }
 }
